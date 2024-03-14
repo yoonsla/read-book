e14ec900cc723d9dd1d37af0ea3f805c9f126259
yoonsla <sangyoon5_4@naver.com>
2024-03-14 09:48:29 +0900

    init

---
+++ src/test/java/com/example/readbook/ReadBookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.readbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReadBookApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
